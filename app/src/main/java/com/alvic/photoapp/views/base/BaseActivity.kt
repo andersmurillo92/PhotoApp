aaa2f3c7843ddcb989660655a1fed0fbbcf2611c
John Anders Murillo <jamsmurillo92@gmail.com>
2022-11-01 06:38:41 -0500

    Create base activity

--- app/src/main/java/com/alvic/photoapp/views/base/BaseActivity.kt
+++ app/src/main/java/com/alvic/photoapp/views/base/BaseActivity.kt
@@ -4,9 +4,14 @@ import android.app.Activity
 import android.app.ProgressDialog
 import android.content.Context
 import android.net.ConnectivityManager
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.alvic.photoapp.R
+import kotlinx.android.synthetic.main.layout_simple_toast.view.*
 
 open class BaseActivity: AppCompatActivity() {
 
@@ -19,6 +24,28 @@ open class BaseActivity: AppCompatActivity() {
             view = View(activity)
         }
         imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    fun showProgressDialog(title: String?, message: String, indeterminate: Boolean, cancelable: Boolean){
+        progressDialog = ProgressDialog.show(this@BaseActivity, title, message, indeterminate, cancelable)
+    }
+
+    fun hideProgressDialog() {
+        if(progressDialog.isShowing)
+            progressDialog.dismiss()
+    }
+
+    fun showSimpleToast(content: String) {
+
+        val inflater = LayoutInflater.from(this@BaseActivity)
+        val layout = inflater.inflate(R.layout.layout_simple_toast, null)
+        layout.tv_ad.text = content
+
+        val mToast = Toast(this@BaseActivity)
+        mToast.setGravity(Gravity.CENTER, 0, 0)
+        mToast.duration = Toast.LENGTH_LONG
+        mToast.view = layout
+        mToast.show()
     }
 
     fun hasNetworkConnection(): Boolean {
