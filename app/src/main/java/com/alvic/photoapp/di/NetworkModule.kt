0fc757c8b43e5703652255ce9fa729db0a434677
John Anders Murillo <jamsmurillo92@gmail.com>
2022-11-01 09:34:12 -0500

    Add network module to be injected

---
+++ app/src/main/java/com/alvic/photoapp/di/NetworkModule.kt
@@ -0,0 +1,41 @@
+package com.alvic.photoapp.di
+
+import com.alvic.photoapp.network.ApiClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        val baseUrl = "https://jsonplaceholder.typicode.com/"
+
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMovieApiClient(retrofit: Retrofit): ApiClient
+        = retrofit.create(ApiClient::class.java)
+}
